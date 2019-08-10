0d6d92ece0f9e9f67db383c2d29fdadd27d93788
calebdeji <calebdeji06@gmail.com>
2019-08-10 19:39:15 +0100

    Done with everything simple stuff needed to be done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 const getData = () => {
+    /**
+     * the json format of the api fecthed
+     */
     let dataFetched;
+    /**
+     * to fetch the api
+     */
     fetch("https://ghibliapi.herokuapp.com/films").then((response) => {
         return response.json();
     }).then((data) => {
-        dataFetched = data;
+        dataFetched = data; //initialized dataFetched
+
+        data.forEach(element => {
+            const rootElement = document.getElementById("root");
+            const cardElement = document.createElement("div");
+            cardElement.setAttribute("class", "card");
+            const divContainingHeader = document.createElement("div");
+            divContainingHeader.setAttribute("class", "header");
+            const divContainingParagraph = document.createElement("div");
+            divContainingParagraph.setAttribute("class", "para");
+            const headerOfCardElement = document.createElement("h3");
+            const bodyOfCardElement = document.createElement("p");
+            headerOfCardElement.textContent = element.title;
+            bodyOfCardElement.textContent = `${element.description.substring(0, 500)}...`;
+            divContainingHeader.appendChild(headerOfCardElement);
+            divContainingParagraph.appendChild(bodyOfCardElement);
+            cardElement.appendChild(divContainingHeader);
+            cardElement.appendChild(divContainingParagraph);
+            rootElement.appendChild(cardElement);
+        });
     })
 }
 
